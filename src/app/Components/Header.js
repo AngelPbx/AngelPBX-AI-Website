3a54ff54cfeee692f57c65b1d2d1a11f39fa60c4
webviosystem10 <webviosystem10@gmail.com>
2025-07-14 17:21:13 +0530

    Update Header.js

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -1,12 +1,13 @@
 "use client";
 import Link from 'next/link';
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import logo from '@/Assets/images/site-logo.png';
 import { useSwiper } from '../SwiperContext';
 
 function Header() {
     const { swiperRef } = useSwiper();
+    const [isShowHead, setIsShowHead] = useState(true);
     const handleLinkClick = (param) => {
         const section = document.getElementById('products');
         if (section) {
@@ -20,16 +21,16 @@ function Header() {
 
     return (
         <div className='fixedTop'>
-            <div className='topHead'>
+            {isShowHead && <div className='topHead'>
                 <div className='row w-100'>
                     <div className='col-xxl-11 col-xl-10 col-lg-10 col-md-10 col-sm-10 col-9 text-center align-content-center'>
                         <p><span>Built for How You Work</span>: AI-Powered Hub for Voice, Video, Text, Tracking and more.</p>
                     </div>
-                    {/* <div className='col-xxl-1 col-xl-2 col-lg-2 col-md-2 col-sm-2 col-3 text-start text-xl-end text-lg-end text-end text-xxl-start'>
-                        <button className='freshBtn '><i className="fa-regular fa-xmark"></i></button>
-                    </div> */}
+                    <div className='col-xxl-1 col-xl-2 col-lg-2 col-md-2 col-sm-2 col-3 text-start text-xl-end text-lg-end text-end text-xxl-start'>
+                        <button className='freshBtn ' onClick={() => setIsShowHead(false)}><i className="fa-regular fa-xmark"></i></button>
+                    </div>
                 </div>
-            </div>
+            </div>}
 
             <nav className="navbar navbar-expand-lg">
                 <div className="container">
