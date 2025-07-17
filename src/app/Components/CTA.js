fc05ddc1a3d870a9f3d69494949c8b2d9c018069
webviosystem8 - Pratima <pratima.majumder@webviotechnologies.com>
2025-07-17 16:06:59 +0530

    update

--- src/app/Components/CTA.js
+++ src/app/Components/CTA.js
@@ -69,112 +69,131 @@ function CTA() {
                                 <button className='secondaryBtn'>Contact Sales</button>
                             </div>
                         </div>
-                       
-                    </div>
-                </div>
-
-                <Image alt="background" className='bg' src={'/images/cta/Background_grad.svg'} width={1440} height={500} priority />
-            </section>
-            <section className='cta mobileCta'>
-                <div className='circleBg'>
-                    {/* <Image alt="logo" src={'/images/cta/CTA.png'} width={767} height={460} /> */}
-                    <Image alt="logo" src={'/images/cta/circleBg.svg'} width={767} height={460} />
-                </div>
-
-                <div className='row align-items-center'>
-                    <div className='col-12 text-center'>
-                        <div className='textWrapper'>
-                            <h3>Ready to power your <br /> business <span className='textGradient'>calls with AI?</span></h3>
-                        </div>
-                        <div className='d-flex btnGroup'>
-                            <button className="primaryBtn me-3">Get a Demo
-                                <span className='arrow'>
-                                    <i className="fa-solid fa-arrow-right"></i>
-                                </span>
-                            </button>
-                            <button className='secondaryBtn'>Contact Sales</button>
-                        </div>
-                    </div>
-                    <div className='col-xl-12'>
-                        <div className='d-flex justify-content-center flex-column align-items-center'>
-                            <div className='logoWrapper'>
-                                <Image alt="logo" src={'/images/cta/Shape.png'} width={370} height={400} />
+                        {/* <div className='col-xl-6'>
+                            <div className='chipWrapper'>
+                                <div className='chip'>
+                                    SmartDial AI
+                                </div>
+                                <div className='chip'>
+                                    AI Powered SmartPBX
+                                </div>
+                                <div className='chip'>
+                                    Video and Voice Intelligence
+                                </div>
+                                <div className='chip'>
+                                    Omnichannel Messaging
+                                </div>
+                                <div className='chip'>
+                                    Personalize AI Agent
+                                </div>
                             </div>
-                            <Swiper
-                                spaceBetween={20}
-                                // slidesPerView={3}
-                                // slidesToScroll={1}
-                                slidesPerView={"auto"}
-                                speed={3000}
-                                loop={true}
-                                autoplay={{
-                                    delay: 0,
-                                    disableOnInteraction: false,
-                                }}
-                                centeredSlides={false}
-                                breakpoints={{
-                                    640: {
-                                        slidesPerView: 3,
-                                        spaceBetween: 20,
-                                    },
-                                    768: {
-                                        slidesPerView: 4,
-                                        spaceBetween: 20,
-                                    },
-                                    1024: {
-                                        slidesPerView: 5,
-                                        spaceBetween: 20,
-                                    },
-                                    1920: {
-                                        slidesPerView: 7,
-                                        spaceBetween: 20,
-                                    },
-                                }}
-                                pagination={false}
-                                navigation={false}
-                                modules={[Autoplay]}
-                                className="mySwiper"
-                            >
-                                <SwiperSlide className="chipWrapper">
-                                    <div className='chip'>
-                                        {allProducts[0].name}
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className="chipWrapper">
-                                    <div className='chip'>
-                                        {allProducts[1].name}
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className="chipWrapper">
-                                    <div className='chip'>
-                                        {allProducts[2].name}
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className="chipWrapper">
-                                    <div className='chip'>
-                                        {allProducts[3].name}
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className="chipWrapper">
-                                    <div className='chip'>
-                                        {allProducts[4].name}
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className="chipWrapper">
-                                    <div className='chip'>
-                                        {allProducts[5].name}
-                                    </div>
-                                </SwiperSlide>
-                            </Swiper>
-                        </div>
+                            <div className='logoWrapper'>
+                                {/* <div className='apbx-logo' /> 
+                                <Image alt="logo" src={'/images/cta/apbx-static.svg'} width={400} height={400} />
+                            </div>
 
+                        </div> */}
                     </div>
                 </div>
 
                 <Image alt="background" className='bg' src={'/images/cta/Background_grad.svg'} width={1440} height={500} priority />
             </section>
 
-            
+            <section className='cta mobileCta'>
+                <div className='container h-100'>
+                    <div className='row h-100 align-items-center'>
+                        <div className='col-12 text-center'>
+                            <div className='textWrapper'>
+                                <h3>Ready to power your <br /> business <span className='textGradient'>calls with AI?</span></h3>
+                            </div>
+                            <div className='d-flex btnGroup'>
+                                <button className="primaryBtn me-3">Get a Demo
+                                    <span className='arrow'>
+                                        <i className="fa-solid fa-arrow-right"></i>
+                                    </span>
+                                </button>
+                                <button className='secondaryBtn'>Contact Sales</button>
+                            </div>
+                        </div>
+                        <div className='col-xl-12'>
+                            <div className='d-flex justify-content-center flex-column align-items-center'>
+                                <div className='logoWrapper'>
+                                    <Image alt="logo" src={'/images/cta/Shape.png'} width={370} height={400} />
+                                </div>
+                                <Swiper
+                                    spaceBetween={20}
+                                    // slidesPerView={3}
+                                    slidesPerView={"auto"}
+                                    // slidesToScroll={1}
+                                    speed={3000}
+                                    loop={true}
+                                    autoplay={{
+                                        delay: 0,
+                                        disableOnInteraction: false,
+                                    }}
+                                    centeredSlides={false}
+                                    breakpoints={{
+                                        640: {
+                                            slidesPerView: 3,
+                                            spaceBetween: 20,
+                                        },
+                                        768: {
+                                            slidesPerView: 4,
+                                            spaceBetween: 20,
+                                        },
+                                        1024: {
+                                            slidesPerView: 5,
+                                            spaceBetween: 20,
+                                        },
+                                        1920: {
+                                            slidesPerView: 7,
+                                            spaceBetween: 20,
+                                        },
+                                    }}
+                                    pagination={false}
+                                    navigation={false}
+                                    modules={[Autoplay]}
+                                    className="mySwiper"
+                                >
+                                    <SwiperSlide className="chipWrapper">
+                                        <div className='chip'>
+                                            {allProducts[0].name}
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide className="chipWrapper">
+                                        <div className='chip'>
+                                            {allProducts[1].name}
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide className="chipWrapper">
+                                        <div className='chip'>
+                                            {allProducts[2].name}
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide className="chipWrapper">
+                                        <div className='chip'>
+                                            {allProducts[3].name}
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide className="chipWrapper">
+                                        <div className='chip'>
+                                            {allProducts[4].name}
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide className="chipWrapper">
+                                        <div className='chip'>
+                                            {allProducts[5].name}
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <Image alt="background" className='bg' src={'/images/cta/cta-mobile-bg.svg'} width={412} height={636} priority />
+            </section>
 
         </>
     )
