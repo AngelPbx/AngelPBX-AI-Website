293969df9697a63c9be4113b56bf901b4f7c35d8
webviosystem10 <webviosystem10@gmail.com>
2025-07-17 10:21:22 +0530

    Comp table updated

--- src/app/Components/Comparision.js
+++ src/app/Components/Comparision.js
@@ -19,7 +19,7 @@ const Comparision = () => {
                                     <div className='listing_hed'>
                                         <h4 className='heading4'>Key Features & Capabilities </h4>
                                     </div>
-                                    <ul className='ps-0'>
+                                    <ul className='ps-0' style={{ paddingTop: '29px' }}>
                                         <li className='feature_listing'><p>Smart Cloud PBX  <span>Click to call, Smart Call Routing instantly, Visual IVR  </span></p></li>
                                         <li className='feature_listing'><p>Agentic AI Agents   <span>Conversational AI that functions like human  </span></p></li>
                                         <li className='feature_listing'><p>OmniChannel Messaging Hub  <span>Unified Inbox with Internal messaging  </span></p></li>
@@ -35,7 +35,7 @@ const Comparision = () => {
                                         <Image src="/images/Top-logo.svg" alt="logo" width={96} height={120} className='mobile_futureLogo' />
                                     </div>
                                     <ul className='ps-0'>
-                                        <li className='feature_listing'> <Image src="/images/tick.svg" alt="tick" width={42} height={42} /></li>
+                                        <li className='feature_listing listing_flexBox'> <Image src="/images/tick.svg" alt="tick" width={42} height={42} /> <span>Semantic Search with CDR</span></li>
                                         <li className='feature_listing'><Image src="/images/tick.svg" alt="tick" width={42} height={42} /></li>
                                         <li className='feature_listing'><Image src="/images/tick.svg" alt="tick" width={42} height={42} /></li>
                                         <li className='feature_listing'><Image src="/images/tick.svg" alt="tick" width={42} height={42} /></li>
